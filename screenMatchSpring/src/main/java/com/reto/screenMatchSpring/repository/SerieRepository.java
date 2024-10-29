2656bb36243699903d46e756fb0168ddbd51584a
Julian <julian.lopezvel@gmail.com>
2024-10-29 16:05:57 -0500

    Derived Queries & JPQL methods

--- screenMatchSpring/src/main/java/com/reto/screenMatchSpring/repository/SerieRepository.java
+++ screenMatchSpring/src/main/java/com/reto/screenMatchSpring/repository/SerieRepository.java
@@ -1,4 +1,27 @@
 package com.reto.screenMatchSpring.repository;
 
-public interface SerieRepository {
+import com.reto.screenMatchSpring.model.Episode;
+import com.reto.screenMatchSpring.model.Genre;
+import com.reto.screenMatchSpring.model.Serie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface SerieRepository extends JpaRepository<Serie, Long> {
+    List<Serie> findTop10ByOrderByRatingDesc();
+
+    List<Serie> findByGenre(Genre genre);
+
+    @Query("SELECT s FROM Serie s WHERE s.totalSeasons <= :totalSeasons")
+    List<Serie> seriesBySeasonsNumber(int totalSeasons);
+
+    @Query("SELECT s FROM Serie s WHERE s.rating <= :ratingNumber")
+    List<Serie> seriesByRating(double ratingNumber);
+
+    Optional<Serie> findByTitleContainsIgnoreCase(String serieName);
+
+    @Query("SELECT e FROM Serie s JOIN s.episodes e WHERE s = :serie ORDER BY e.rating DESC LIMIT 10")
+    List<Episode> top10Episodes(Serie serie);
 }
