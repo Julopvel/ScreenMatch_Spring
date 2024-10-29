f7d506df4e3195b8ea324e1f394535071acf0b69
Julian <julian.lopezvel@gmail.com>
2024-10-29 15:58:46 -0500

    Added info relative to the Record.

--- screenMatchSpring/src/main/java/com/reto/screenMatchSpring/model/DataSeasons.java
+++ screenMatchSpring/src/main/java/com/reto/screenMatchSpring/model/DataSeasons.java
@@ -1,4 +1,15 @@
 package com.reto.screenMatchSpring.model;
 
-public record DataSeasons() {
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DataSeasons(
+        @JsonAlias("Season")
+        Integer temporada,
+        @JsonAlias("Episodes")
+        List<DataEpisode> episodios
+) {
 }
